cb8287800ce1e6fb5fd40089bab89e2c9b30f19b
shubo7996 <whatsupmofo420@gmail.com>
2019-01-11 08:29:23 +0530

    UserLogin

--- apiService/views.py
+++ apiService/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .serializers import UserSerializer
+from .serializers import UserSerializer,UserLoginSerializer
 from django.contrib.auth import get_user_model
 from rest_framework.generics import (
     CreateAPIView,
@@ -14,10 +14,26 @@ from rest_framework.permissions import (
     IsAdminUser,
     IsAuthenticatedOrReadOnly
 )
-# Create your views here.
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
 
 User = get_user_model()
 
 class UserCreateApiView(CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+class UserCreateLoginView(APIView):
+    permission_classes = [AllowAny]
+    serializer_class = UserLoginSerializer
+
+    def post(self,request,format=None):
+        serializer = UserLoginSerializer(data=request.data)
+        if serializer.is_valid():
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
