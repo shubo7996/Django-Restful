d468ae6324c65a9aad3c64e36cd91387b9feaa06
shubo7996 <whatsupmofo420@gmail.com>
2019-01-11 08:35:31 +0530

    api/user/login added

--- apiService/api_urls.py
+++ apiService/api_urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
 from .api_views import SongsList,SongsDetail
-from .views import UserCreateApiView
+from .views import UserCreateApiView,UserCreateLoginView
 from rest_framework.urlpatterns import format_suffix_patterns
 
 
 urlpatterns = [
     path('songs/', SongsList.as_view()),
     path('songs/<int:pk>/', SongsDetail.as_view()),
-    path('users/register/', UserCreateApiView.as_view()) 
+    path('users/register/', UserCreateApiView.as_view()) ,
+    path('users/login',UserCreateLoginView.as_view())
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
